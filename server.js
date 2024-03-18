cd11f7f1f9fc50c82042e7b4f456a3f760930fad
suryaveeramani0044 <suryaveeramani0044@gmail.com>
2024-03-18 18:39:50 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const app = require("express")();
+
+const PORT = process.env.PORT || 8000;
+
+//middleware
+
+//route
+
+//server
+app.listen(PORT, () => {
+  console.log(`server is running ${PORT}`);
+});
